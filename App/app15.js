c9291543429b1689a5e2f3d598add4e2c141e824
Shital Patel <shital732@yahoo.com>
2020-03-14 15:43:43 -0400

    solved rank-activity

--- App/app15.js
+++ App/app15.js
@@ -36,3 +36,41 @@ Note: Codewars no longer uses this algorithm for its own ranking system. It uses
 
 
  */
+
+function User() {
+  this.rank = -8;
+  this.progress = 0;
+  this.hundred = 100;
+  this.highest = 8;
+}
+
+User.prototype.incProgress = function(rank) {
+  if (rank == 0 || rank > this.highest || rank < -this.highest)
+    throw new RangeError("rank input out of range");
+  if (this.rank == this.highest) return;
+
+  var diff =
+    (rank > 0 && this.rank < 0) || (rank < 0 && this.rank > 0)
+      ? Math.abs(this.rank) + Math.abs(rank)
+      : rank - this.rank;
+
+  if (rank > 0 && this.rank < 0) diff--;
+  if (rank < 0 && this.rank > 0) diff = -diff;
+
+  if (diff > 0) {
+    this.progress += rank == 1 && this.rank == -1 ? 10 : 10 * diff * diff;
+  } else {
+    this.progress += diff == 0 ? 3 : 1;
+  }
+
+  if (this.progress > this.hundred && this.rank < this.highest) {
+    this.rank += Math.floor(this.progress / this.hundred);
+
+    if (this.rank == 0) this.rank++;
+    this.progress %= this.hundred;
+  }
+
+  if (this.rank == this.highest) this.progress = 0;
+
+  return diff;
+};
